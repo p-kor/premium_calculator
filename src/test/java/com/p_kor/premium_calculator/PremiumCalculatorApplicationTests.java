d65b2603b1bd8f8e626b316e3669058ad8b260bf
Pavel Korolev <pasha.korolev@gmail.com>
2025-02-05 12:01:45 +0100

    test: refactor acceptance tests for using beans from Context

--- src/test/java/com/p_kor/premium_calculator/PremiumCalculatorApplicationTests.java
+++ src/test/java/com/p_kor/premium_calculator/PremiumCalculatorApplicationTests.java
@@ -2,8 +2,9 @@ package com.p_kor.premium_calculator;
 
 import com.p_kor.premium_calculator.model.*;
 import com.p_kor.premium_calculator.service.TypeRiskStrategyService;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
@@ -11,29 +12,36 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@SpringBootTest
 class PremiumCalculatorApplicationTests {
-    private PremiumCalculator premiumCalculator;
 
-    @BeforeEach
-    void init() {
-        TypeRiskStrategyService typeRiskStrategyService = new TypeRiskStrategyService();
-        premiumCalculator = new PremiumCalculator(typeRiskStrategyService);
-    }
+    @Autowired
+    private TypeRiskStrategyService typeRiskStrategyService;
+
+    @Autowired
+    private PremiumCalculator premiumCalculator;
 
     @Test
     void acceptanceTest1() {
+        BigDecimal EXPECTED_RESULT = new BigDecimal("2.28");
+
         List<PolicySubObject> subObjects = List.of(
                 new PolicySubObject("TV", new BigDecimal("100.0"), RiskType.FIRE),
                 new PolicySubObject("Door bell", new BigDecimal("8.0"), RiskType.THEFT));
+
         List<PoliceObject> objects = List.of(
                 new PoliceObject("Cottage", subObjects));
+
         Policy policy = new Policy("LV20-02-100000-5", PolicyStatus.APPROVED, objects);
+
         BigDecimal premium = premiumCalculator.calculate(policy);
-        assertEquals(new BigDecimal("2.28"), premium, "Premium should be 2.28");
+        assertEquals(EXPECTED_RESULT, premium, "Premium should be 2.28");
     }
 
     @Test
     void acceptanceTest2() {
+        BigDecimal EXPECTED_RESULT = new BigDecimal("17.13");
+
         List<PolicySubObject> subObjectsFlat47 = List.of(
                 new PolicySubObject("Air conditioner", new BigDecimal("500.0"), RiskType.FIRE));
         List<PolicySubObject> subObjectsHouse11 = List.of(
@@ -46,11 +54,13 @@ class PremiumCalculatorApplicationTests {
         Policy policy = new Policy("LV20-02-100000-6", PolicyStatus.APPROVED, objects);
 
         BigDecimal premium = premiumCalculator.calculate(policy);
-        assertEquals(new BigDecimal("17.13"), premium, "Premium should be 17.13");
+        assertEquals(EXPECTED_RESULT, premium, "Premium should be 17.13");
     }
 
     @Test
     void acceptanceTest3() {
+        BigDecimal EXPECTED_RESULT = new BigDecimal("17.13");
+
         List<PolicySubObject> subObjectsFlat47 = List.of(
                 new PolicySubObject("Air conditioner", new BigDecimal("500.0"), RiskType.FLOOD));
         List<PolicySubObject> subObjectsHouse11 = List.of(
@@ -60,9 +70,9 @@ class PremiumCalculatorApplicationTests {
         objects.add(new PoliceObject("Flat 47", subObjectsFlat47));
         objects.add(new PoliceObject("House 11", subObjectsHouse11));
 
-        Policy policy = new Policy("LV20-02-100000-6", PolicyStatus.APPROVED, objects);
+        Policy policy = new Policy("#@$@!-1", PolicyStatus.REGISTERED, objects);
 
         BigDecimal premium = premiumCalculator.calculate(policy);
-        assertEquals(new BigDecimal("17.13"), premium, "Premium should be 17.13");
+        assertEquals(EXPECTED_RESULT, premium, "Premium should be 17.13");
     }
 }
