7aa0b54ac70746dc4c25fb33d67b577af668b094
Pavel Korolev <pasha.korolev@gmail.com>
2025-02-05 11:49:37 +0100

    test: refactor test

--- src/test/java/com/p_kor/premium_calculator/PremiumCalculatorApplicationTests.java
+++ src/test/java/com/p_kor/premium_calculator/PremiumCalculatorApplicationTests.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -33,11 +34,32 @@ class PremiumCalculatorApplicationTests {
 
     @Test
     void acceptanceTest2() {
-        List<PolicySubObject> subObjects = List.of(
-                new PolicySubObject("Air conditioner", new BigDecimal("500.0"), RiskType.FIRE),
+        List<PolicySubObject> subObjectsFlat47 = List.of(
+                new PolicySubObject("Air conditioner", new BigDecimal("500.0"), RiskType.FIRE));
+        List<PolicySubObject> subObjectsHouse11 = List.of(
                 new PolicySubObject("Oven", new BigDecimal("102.51"), RiskType.THEFT));
-        List<PoliceObject> objects = List.of(
-                new PoliceObject("Flat", subObjects));
+
+        List<PoliceObject> objects = new ArrayList<>();
+        objects.add(new PoliceObject("Flat 47", subObjectsFlat47));
+        objects.add(new PoliceObject("House 11", subObjectsHouse11));
+
+        Policy policy = new Policy("LV20-02-100000-6", PolicyStatus.APPROVED, objects);
+
+        BigDecimal premium = premiumCalculator.calculate(policy);
+        assertEquals(new BigDecimal("17.13"), premium, "Premium should be 17.13");
+    }
+
+    @Test
+    void acceptanceTest3() {
+        List<PolicySubObject> subObjectsFlat47 = List.of(
+                new PolicySubObject("Air conditioner", new BigDecimal("500.0"), RiskType.FLOOD));
+        List<PolicySubObject> subObjectsHouse11 = List.of(
+                new PolicySubObject("Oven", new BigDecimal("102.51"), RiskType.THEFT));
+
+        List<PoliceObject> objects = new ArrayList<>();
+        objects.add(new PoliceObject("Flat 47", subObjectsFlat47));
+        objects.add(new PoliceObject("House 11", subObjectsHouse11));
+
         Policy policy = new Policy("LV20-02-100000-6", PolicyStatus.APPROVED, objects);
 
         BigDecimal premium = premiumCalculator.calculate(policy);
